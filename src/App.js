a71aaf274cccd521d812dedadc27266460bd50a1
Prashant <prashantkarki630@gmail.com>
2023-04-26 16:35:20 +0545

    navbar and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <nav className="navbar">
+        <div className="navbar-content">
+          <input
+            className="url-input"
+            type="text"
+            placeholder="Paste the URL"
+            autoFocus
+          />
+          <button className="submit-btn" type="submit">
+            Screenshot
+          </button>
+        </div>
+      </nav>
+
+      <section className="hero"></section>
     </div>
   );
 }
